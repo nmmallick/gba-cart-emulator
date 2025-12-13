2a7ffcde5201c741ffc833eb841420d097571222
malloc <nmm109@pitt.edu>
2025-12-13 12:34:43 -0500

    configure sys clock to run at 84mhz

--- src/main.c
+++ src/main.c
@@ -2,25 +2,36 @@
 #include <systick.h>
 #include <clock.h>
 
-// RCC
-#define RCC_BASE_ADDR 0x40023800
-#define RCC MMIO32(RCC_BASE_ADDR)
-#define RCC_AHB1ENR MMIO32(RCC_BASE_ADDR + 0x30)
-
 static uint32_t count = 0;
 static uint8_t value = 0;
 
 void SystemInit() {
-    // TODO: Clock initialization
+    // Set PLL M=8, N=84, P=2
+    SET_PLLM(8);
+    SET_PLLN(84);
+    SET_PLLP(2);
+
+    // Set PLL clock input source to HSI
+    // Turn on PLL and HSI clocks and turn off HSE for power consumption
+    RCC->RCC_CR |= PLL_ON | HSI_ON;
+    RCC->RCC_CR &= HSE_OFF;
+
+    while (!PLL_IS_RDY());
+
+    // Set the system clock to use the PLL clock
+    RCC->RCC_CFGR |= SYS_CLOCK_PLL;
+
+    while (((RCC->RCC_CFGR >> 2) & 0x3) != SYS_CLOCK_PLL);
+
     // Enable clock for GPIOA
-    *RCC_AHB1ENR |= 0x01;
+    RCC->RCC_AHB1ENR |= 0x01;
 
     // setup gpio pin A5
     set_gpio_mode(GPIOA, GPIO_MODE_OUT, 5);
     set_gpio_otype(GPIOA, GPIO_OTYPE_PP, 5);
     set_gpio_ospeed(GPIOA, GPIO_SPEED_LOW, 5);
     set_gpio_pupdr(GPIOA, GPIO_PUPDR_PD, 5);
-    set_gpio_output(GPIOA, GPIO_LOW, 5);
+    set_gpio_output(GPIOA, GPIO_HIGH, 5);
 
     // setup systick
     SYSTICK->STK_LOAD = SYSTICK_RELOAD_RESET;
@@ -32,8 +43,8 @@ void SystemInit() {
 
 void SysTick_Handler() {
     count++;
-
-    if (count == 100000) {
+ 
+    if (count == 1000) {
 	if (value) {
 	    set_gpio_output(GPIOA, GPIO_LOW, 5);
 	    value = 0;
